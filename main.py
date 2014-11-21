52414f3a416b67a60bded6fa6fea4fa2e3692255
Emily Shepherd <emily@emilyshepherd.me>
2014-11-21 02:44:21 +0000

    Added stub methods and basic database structures

--- main.py
+++ main.py
@@ -15,11 +15,75 @@
 # limitations under the License.
 #
 import webapp2
+import json
+from google.appengine.ext import ndb
 
-class MainHandler(webapp2.RequestHandler):
+class User(ndb.Model):
+    id = ndb.IntegerProperty()
+    name = ndb.StringProperty()
+
+class Game(ndb.Model):
+    id = ndb.IntegerProperty()
+    name = ndb.StringProperty()
+    members = ndb.IntegerProperty(repeated=True)
+
+class JSONHandler(webapp2.RequestHandler):
+    def __init__(self, request, response):
+        super(JSONHandler, self).__init__(request, response)
+
+        self.response.content_type = 'application/json'
+        self.json = { };
+
+    def __del__(self):
+        self.response.write(json.dumps(self.json))
+
+class UserHandler(JSONHandler):
+
+    # /user/
     def get(self):
-        self.response.write('<h1>CAD Team M</h1>')
+        self.json['test'] = 'foo'
+
+class GameHandler(JSONHandler):
+
+    # GET /game/
+    #
+    # Returns a list of games
+    def get(self):
+        self.json['games'] = { }
+
+    # GET /game/start/<id>
+    #
+    # Starts a game
+    def start(self, id):
+        self.json['id'] = id
+        self.json['status'] = 'started'
+
+    # GET /game/join/<id>
+    #
+    # Joins a game
+    def join(self, id):
+        self.json['id'] = id
+        self.json['status'] = 'joined'
+
+    # GET /game/before/<id>
+    #
+    # 
+    def before(self, id):
+        self.json['id'] = id
+        self.json['status'] = 'ok'
+
+    # PUT /game/
+    #
+    # Creates a game
+    def create(self):
+        self.json['status'] = 'created'
+        self.json['id'] = 13
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    webapp2.Route(r'/user',             handler=UserHandler),
+    webapp2.Route(r'/game/',            handler=GameHandler,                          methods=['GET']),
+    webapp2.Route(r'/game/start/<id>',  handler=GameHandler, handler_method='start'),
+    webapp2.Route(r'/game/join/<id>',   handler=GameHandler, handler_method='join'),
+    webapp2.Route(r'/game/before/<id>', handler=GameHandler, handler_method='start'),
+    webapp2.Route(r'/game/',            handler=GameHandler, handler_method='create', methods=['PUT'])
 ], debug=True)

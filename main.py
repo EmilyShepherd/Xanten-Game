2e06a13fa5ba36b549a365b1a64da520232b5c4e
Emily Shepherd <emily@emilyshepherd.me>
2014-11-21 18:35:40 +0000

    Refactor and /game/<gid> now returns a status along with the list of users

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class Game(ndb.Model):
     owner = ndb.StringProperty()
     private = ndb.BooleanProperty(default=True)
     gmap = ndb.StringProperty()
+    running = ndb.BooleanProperty(default=False)
 
 # Handles all requests
 #
@@ -103,15 +104,18 @@ class GameHandler(DefaultHandler):
         self.json['id']     = id
         self.json['status'] = 'ok'
 
-    def users(self, gid):
+    def get_game(self, gid):
         query = Game.query(Game.gid == gid)
 
         if query.count() != 1:
             self.stderr('Unknown Game')
         else:
-            self.json['users'] = [ ]
+            game = query.fetch(1)[0]
+            
+            self.json['status'] = 'running' if game.running else 'waiting'
+            self.json['users']  = [ ]
 
-            for uid in query.fetch(1)[0].members:
+            for uid in game.members:
                 user = User.query(User.uid == uid).fetch(1)[0]
                 self.json['users'].append(user.name)
 
@@ -141,10 +145,10 @@ class GameHandler(DefaultHandler):
 
 # Setup routes
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/game/<gid>/users',  handler=GameHandler, handler_method='users'),
+    webapp2.Route(r'/game/<gid>',        handler=GameHandler, handler_method='get_game'),
     webapp2.Route(r'/game/',             handler=GameHandler,                          methods=['GET']),
-    webapp2.Route(r'/game/start/<gid>',  handler=GameHandler, handler_method='start'),
-    webapp2.Route(r'/game/join/<gid>',   handler=GameHandler, handler_method='join'),
-    webapp2.Route(r'/game/before/<gid>', handler=GameHandler, handler_method='start'),
+    webapp2.Route(r'/game/<gid>/start',  handler=GameHandler, handler_method='start'),
+    webapp2.Route(r'/game/<gid>/join',   handler=GameHandler, handler_method='join'),
+    webapp2.Route(r'/game/<gid>/before', handler=GameHandler, handler_method='start'),
     webapp2.Route(r'/game/',             handler=GameHandler, handler_method='create', methods=['PUT'])
 ], debug=True)

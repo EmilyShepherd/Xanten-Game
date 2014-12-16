198afbb51eb5b8f38561df151e18ac60bfdc5b12
Joseph Buckingham <joe_buckingham@live.co.uk>
2014-12-16 22:33:06 +0000

    General map
    Completely rewritten - should be bug-free now.

--- static/js/game/map.js
+++ static/js/game/map.js
@@ -8,6 +8,16 @@
 	 *
 	 */
 
+	function check(y_coord, x_coord) {
+		// Checks the value of a space in the map array, returning the empty string for co-ordinates that don't exist.
+		// (This avoids nasty array index errors when we get close to the edges.)
+		if (x_coord < 0 || y_coord < 0 || x_coord > 6 || y_coord > 6) {
+			return "";
+		} else {
+			return imgArray[y_coord][x_coord];
+		}
+	}
+
 	function generateGeneralMap(size){
 
 		// Controlling amount of space on the map.
@@ -35,47 +45,29 @@
 		// What's the likelihood that they'll spread?
 		var OCEAN_EXPAND = Math.floor((Math.random() * 9) + 3)/100; // 0.03 --> 0.09
 		var FOREST_EXPAND = Math.floor((Math.random() * 9) + 5)/100; // 0.05 --> 0.09
-	  // Calculating probabilities:
-	  var p_ocean = 1 - OCEAN_SEED;
-	  var p_rock = p_ocean - ROCK_SEED;
-	  var p_forest = p_rock - FOREST_SEED;
+		// Calculating probabilities:
+		var p_ocean = 1 - OCEAN_SEED;
+		var p_rock = p_ocean - ROCK_SEED;
+		var p_forest = p_rock - FOREST_SEED;
 
 		var x = 0;
 		var y = 0;
 		var r = 0;
-		var imgArray = [
-			[],
-			[],
-			[],
-			[],
-			[],
-			[],
-			[]
-		];
-
-		// First sweep, populating the world with trees, rocks and single-block oceans.
-	  while (y < 7) {
-	    while (x < 7) {
-	      r = Math.random();
-	      if ((r > p_ocean) && (noClear > minClear)) {
-	        imgArray[y][x] = "o";
+		var imgArray = [[], [], [], [], [], [], []];
+		while (y < 7) {
+			while (x < 7) {
+				r = Math.random();
+				if ((r > p_ocean) && (noClear > minClear)) {
+					imgArray[y][x] = "o";
 					noClear--;
-	      } else if ((r > p_rock) && (noClear > minClear)) {
-	        imgArray[y][x] = "m".concat(Math.round(10*(r - p_rock)));
-					noClear--;
-	      } else if ((r > p_forest) && (noClear > minClear)) {
-	        imgArray[y][x] = "t".concat(Math.round(10*(r - p_forest)));
-					noClear--;
-	      } else {
-	        imgArray[y][x] = "g";
-	      }
-	      x++;
-	    }
-	    x = 0;
-	    y++;
-	  }
-
-	  // Second sweep, finding single-block oceans and expanding them.
+				} else {
+					imgArray[y][x] = "g";
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
 		x = 0;
 		y = 0;
 		while (y < 7) {
@@ -106,30 +98,44 @@
 								noClear--;
 							}
 						}
-					} else if (imgArray[y][x] === "t0") {
-						if (x > 0) {
-							if (Math.random() < FOREST_EXPAND) {
-								imgArray[y][x - 1] = "t1";
-								noClear--;
-							}
+					}
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+		x = 0;
+		y = 0;
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "g") {
+					if (check(y+1, x) === "o") {
+						if (check(y-1, x) === "o") {
+							imgArray[y][x] = "o";
+						} else {
+							imgArray[y][x] = "o-s";
 						}
-						if (x < 6) {
-							if (Math.random() < FOREST_EXPAND) {
-								imgArray[y][x + 1] = "t1";
-								noClear--;
-							}
+					}
+					if (check(y-1, x) === "o") {
+						if (check(y+1, x) === "o") {
+							imgArray[y][x] = "o";
+						} else {
+							imgArray[y][x] = "o-n";
 						}
-						if (y > 0) {
-								if (Math.random() < FOREST_EXPAND) {
-								imgArray[y - 1][x] = "t1";
-								noClear--;
-							}
+					}
+					if (check(y, x+1) === "o") {
+						if (check(y, x-1) === "o") {
+							imgArray[y][x] = "o";
+						} else {
+							imgArray[y][x] = "o-e";
 						}
-						if (y < 6) {
-							if (Math.random() < FOREST_EXPAND) {
-								imgArray[y + 1][x] = "t1";
-								noClear--;
-							}
+					}
+					if (check(y, x-1) === "o") {
+						if (check(y, x+1) === "o") {
+							imgArray[y][x] = "o";
+						} else {
+							imgArray[y][x] = "o-w";
 						}
 					}
 				}
@@ -138,69 +144,67 @@
 			x = 0;
 			y++;
 		}
-
-	  // Third sweep, adding nice smooth coastline to the oceans.
 		x = 0;
 		y = 0;
 		while (y < 7) {
 			while (x < 7) {
-				if (imgArray[y][x] !== "o") {
-					var n = false;
-					var s = false;
-					var w = false;
-					var e = false;
-					var nw = false;
-					var ne = false;
-					var sw = false;
-					var se = false;
-					if (y > 0) {
-						n = (imgArray[y - 1][x] === "o" || imgArray[y - 1][x] === "ob-se" || imgArray[y - 1][x] === "ob-sw" || imgArray[y - 1][x] === "o-s");
-					}
-					if (y < 6) {
-						s = (imgArray[y + 1][x] === "o" || imgArray[y + 1][x] === "ob-ne" || imgArray[y + 1][x] === "ob-nw" || imgArray[y + 1][x] === "o-n");
-					}
-					if (x > 0) {
-						w = (imgArray[y][x - 1] === "o" || imgArray[y][x - 1] === "ob-ne" || imgArray[y][x - 1] === "ob-se" || imgArray[y][x - 1] === "o-e");
-						if (y > 0) {
-							nw = (imgArray[y - 1][x - 1] === "o");
-						}
-						if (y < 6) {
-							sw = (imgArray[y + 1][x - 1] === "o");
-						}
-					}
-					if (x < 6) {
-						e = (imgArray[y][x + 1] === "o" || imgArray[y][x + 1] === "ob-nw" || imgArray[y][x + 1] === "ob-sw" || imgArray[y][x + 1] === "o-w");
-						if (y > 0) {
-							ne = (imgArray[y - 1][x + 1] === "o");
-						}
-						if (y < 6) {
-							se = (imgArray[y + 1][x + 1] === "o");
-						}
-					}
-					if ((n && s) || (w && e)) {
-						imgArray[y][x] = "o";
-						x = 0;
-						y = 0;
-					} else if (n && !w && !e && !s) {
-						imgArray[y][x] = "o-n";
-					} else if (!n && !w && !e && s) {
-						imgArray[y][x] = "o-s";
-					} else if (!n && w && !e && !s) {
-						imgArray[y][x] = "o-w";
-					} else if (!n && !w && e && !s) {
-						imgArray[y][x] = "o-e";
-					} else if (nw && se && !n && !s && !e && !w) {
-						imgArray[y][x] = "oa-nw";
-					} else if (sw && ne && !n && !s && !e && !w) {
-						imgArray[y][x] = "oa-ne";
-					} else if (nw && !n && !w) {
-						imgArray[y][x] = "oc-nw";
-					} else if (ne && !n && !e) {
+				if (check(y, x) === "g") {
+					if (check(y+1, x) === "o-e" && check(y, x+1) === "o-s") imgArray[y][x] = "oc-se";
+					if (check(y+1, x) === "o-w" && check(y, x-1) === "o-s") imgArray[y][x] = "oc-sw";
+					if (check(y-1, x) === "o-e" && check(y, x+1) === "o-n") imgArray[y][x] = "oc-ne";
+					if (check(y-1, x) === "o-w" && check(y, x-1) === "o-n") imgArray[y][x] = "oc-nw";
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+		x = 0;
+		y = 0;
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "o-e") {
+					if (check(y, x-1) === "o-n") imgArray[y][x] = "ob-ne";
+					if (check(y, x-1) === "o-s") imgArray[y][x] = "ob-se";
+				}
+				if (check(y, x) === "o-w") {
+					if (check(y, x+1) === "o-n") imgArray[y][x] = "ob-nw";
+					if (check(y, x+1) === "o-s") imgArray[y][x] = "ob-sw";
+				}
+				if (check(y, x) === "o-s") {
+					if (check(y-1, x) === "o-e") imgArray[y][x] = "ob-se";
+					if (check(y-1, x) === "o-w") imgArray[y][x] = "ob-sw";
+				}
+				if (check(y, x) === "o-n") {
+					if (check(y+1, x) === "o-e") imgArray[y][x] = "ob-ne";
+					if (check(y+1, x) === "o-w") imgArray[y][x] = "ob-nw";
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+
+		x = 0;
+		y = 0;
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "g") {
+					if (check(y-1, x) === "o-e" && check(y, x+1) === "o-e") {
 						imgArray[y][x] = "oc-ne";
-					} else if (sw && !s && !w) {
-						imgArray[y][x] = "oc-sw";
-					} else if (se && !s && !e) {
+						imgArray[y][x+1] = "ob-ne";
+					}
+					if (check(y-1, x) === "o-w" && check(y, x-1) === "o-w") {
+						imgArray[y][x] = "oc-nw";
+						imgArray[y][x-1] = "ob-nw";
+					}
+					if (check(y+1, x) === "o-e" && check(y, x+1) === "o-e") {
 						imgArray[y][x] = "oc-se";
+						imgArray[y][x+1] = "ob-se";
+					}
+					if (check(y+1, x) === "o-w" && check(y, x-1) === "o-w") {
+						imgArray[y][x] = "oc-sw";
+						imgArray[y][x-1] = "ob-sw";
 					}
 				}
 				x++;
@@ -213,121 +217,130 @@
 		y = 0;
 		while (y < 7) {
 			while (x < 7) {
-				if (x===0 && y===0) {
-					console.log("restart");
+				if (check(y, x) === "o-w") {
+					if (check(y+1, x) === "o") imgArray[y][x] = "ob-sw";
+					if (check(y-1, x) === "o") imgArray[y][x] = "ob-nw";
 				}
-				if (imgArray[y][x] !== "o") {
-					var n = false;
-					var s = false;
-					var w = false;
-					var e = false;
-					var nw = false;
-					var ne = false;
-					var sw = false;
-					var se = false;
-					if (y > 0) {
-						n = (imgArray[y - 1][x] === "o");
+				if (check(y, x) === "o-e") {
+					if (check(y+1, x) === "o") imgArray[y][x] = "ob-se";
+					if (check(y-1, x) === "o") imgArray[y][x] = "ob-ne";
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+		x = 0;
+		y = 0;
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "g") {
+					if (check(y, x-1) === "o-s" && check(y, x+1) === "o-s") imgArray[y][x] = "o-s";
+					if (check(y, x-1) === "o-n" && check(y, x+1) === "o-n") imgArray[y][x] = "o-n";
+					if (check(y-1, x) === "o-e" && check(y+1, x) === "o-e") imgArray[y][x] = "o-e";
+					if (check(y-1, x) === "o-w" && check(y+1, x) === "o-w") imgArray[y][x] = "o-w";
+					if (check(y-1, x) === "o-e" && check(y, x-1) === "o-s") imgArray[y][x] = "ob-se";
+					if (check(y-1, x) === "o-e" && check(y, x+1) === "o-n") imgArray[y][x] = "ob-ne";
+					if (check(y+1, x) === "o-w" && check(y, x-1) === "o-s") imgArray[y][x] = "ob-sw";
+					if (check(y+1, x) === "o-w" && check(y, x+1) === "o-n") imgArray[y][x] = "ob-nw";
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+		x = 0;
+		y = 0;
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "ob-nw" && check(y, x+1) === "ob-nw") imgArray[y][x] = "o";
+				if (check(y, x) === "ob-ne" && check(y, x-1) === "ob-ne") imgArray[y][x] = "o";
+				if (check(y-1, x) === "ob-ne" && check(y, x+1) === "ob-ne") imgArray[y][x] = "oc-ne";
+				if (check(y-1, x) === "ob-nw" && check(y, x-1) === "ob-nw") imgArray[y][x] = "oc-nw";
+				if (check(y+1, x) === "ob-se" && check(y, x+1) === "ob-se") imgArray[y][x] = "oc-se";
+				if (check(y+1, x) === "ob-sw" && check(y, x-1) === "ob-sw") imgArray[y][x] = "oc-sw";
+				if (check(y,x) === "o-w" && check(y, x+1) === "ob-se") imgArray[y][x] = "ob-sw";
+				if (check(y,x) === "o-w" && check(y, x+1) === "ob-ne") imgArray[y][x] = "ob-nw";
+				if (check(y,x) === "o-e" && check(y, x-1) === "ob-nw") imgArray[y][x] = "ob-ne";
+				if (check(y,x) === "o-e" && check(y, x-1) === "ob-sw") imgArray[y][x] = "ob-se";
+				if (check(y,x) === "o-n" && check(y+1, x) === "ob-se") imgArray[y][x] = "ob-ne";
+				if (check(y,x) === "o-n" && check(y+1, x) === "ob-sw") imgArray[y][x] = "ob-nw";
+				if (check(y,x) === "o-s" && check(y-1, x) === "ob-ne") imgArray[y][x] = "ob-sw";
+				if (check(y,x) === "o-s" && check(y-1, x) === "ob-nw") imgArray[y][x] = "ob-se";
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+		x = 0
+		y = 0
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "ob-sw") {
+					if (check(y-1, x) === "o" || check(y-1, x) === "ob-sw" || check(y-1, x) === "ob-se" || check(y-1, x) === "o-s" || check(y, x+1) === "o" || check(y, x+1) === "ob-sw" || check(y, x+1) === "ob-nw" || check(y, x+1) === "o-w") {
+						imgArray[y][x] = "o";
 					}
-					if (y < 6) {
-						s = (imgArray[y + 1][x] === "o");
+				}
+				if (check(y, x) === "ob-nw") {
+					if (check(y+1, x) === "o" || check(y+1, x) === "ob-nw" || check(y+1, x) === "ob-ne" || check(y+1, x) === "o-n" || check(y, x+1) === "o" || check(y, x+1) === "ob-nw" || check(y, x+1) === "ob-sw" || check(y, x+1) === "o-w") {
+						imgArray[y][x] = "o";
 					}
-					if (x > 0) {
-						w = (imgArray[y][x - 1] === "o");
-						if (y > 0) {
-							nw = (imgArray[y - 1][x - 1] === "o");
-						}
-						if (y < 6) {
-							sw = (imgArray[y + 1][x - 1] === "o");
-						}
+				}
+				if (check(y, x) === "ob-se") {
+					if (check(y-1, x) === "o" || check(y-1, x) === "ob-sw" || check(y-1, x) === "ob-se" || check(y-1, x) === "o-s" || check(y, x-1) === "o" || check(y, x-1) === "ob-se" || check(y, x-1) === "ob-ne" || check(y, x-1) === "o-e") {
+						imgArray[y][x] = "o";
 					}
-					if (x < 6) {
-						e = (imgArray[y][x + 1] === "o");
-						if (y > 0) {
-							ne = (imgArray[y - 1][x + 1] === "o");
-						}
-						if (y < 6) {
-							se = (imgArray[y + 1][x + 1] === "o");
-						}
+				}
+				if (check(y, x) === "ob-ne") {
+					if (check(y+1, x) === "o" || check(y+1, x) === "ob-nw" || check(y+1, x) === "ob-ne" || check(y+1, x) === "o-n" || check(y, x-1) === "o" || check(y, x-1) === "ob-ne" || check(y, x-1) === "ob-se" || check(y, x-1) === "o-e") {
+						imgArray[y][x] = "o";
 					}
-					if ((n && (w || sw)) || (w && ne)){
-						if (x === 6 && y === 6) {
-							imgArray[y][x] = "ob-nw";
-						} else if (x === 6) {
-							if (imgArray[y+1][x] === "o-w" || imgArray[y+1][x] === "ob-sw" || imgArray[y+1][x] === "oc-nw" || imgArray[y+1][x] === "oa-nw") {
-								imgArray[y][x] = "ob-nw";
-							}
-						} else if (y === 6) {
-							if (imgArray[y][x+1] === "o-n" || imgArray[y][x+1] === "ob-ne" || imgArray[y][x+1] === "oc-nw" || imgArray[y][x+1] === "oa-nw" ) {
-								imgArray[y][x] = "ob-nw";
-							}
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+		// Straight borders
+		x = 0;
+		y = 0;
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "g") {
+					if (check(y+1, x) === "o") {
+						if (check(y-1, x) === "o") {
+							imgArray[y][x] = "o";
 						} else {
-							if (imgArray[y+1][x] === "o-w" || imgArray[y+1][x] === "ob-sw" || imgArray[y+1][x] === "oc-nw" || imgArray[y+1][x] === "oa-nw") {
-								if (imgArray[y][x+1] === "o-n" || imgArray[y][x+1] === "ob-ne" || imgArray[y][x+1] === "oc-nw" || imgArray[y][x+1] === "oa-nw" ) {
-									imgArray[y][x] = "ob-nw";
-								}
-							}
+							imgArray[y][x] = "o-s";
 						}
 					}
-					if ((n && (e || se)) || (e && nw)){
-						if (x === 0 && y === 6) {
-							imgArray[y][x] = "ob-ne";
-						} else if (x === 0) {
-							if (imgArray[y+1][x] === "o-e" || imgArray[y+1][x] === "ob-se" || imgArray[y+1][x] === "oc-ne" || imgArray[y+1][x] === "oa-ne") {
-								imgArray[y][x] = "ob-ne";
-							}
-						} else if (y === 6) {
-							if (imgArray[y][x-1] === "o-n" || imgArray[y][x-1] === "ob-nw" || imgArray[y][x-1] === "oc-ne" || imgArray[y][x-1] === "oa-ne" ) {
-								imgArray[y][x] = "ob-ne";
-							}
+					if (check(y-1, x) === "o") imgArray[y][x] = "o-n";
+					if (check(y, x+1) === "o") {
+						if (check(y, x-1) === "o") {
+							imgArray[y][x] = "o";
 						} else {
-							if (imgArray[y+1][x] === "o-e" || imgArray[y+1][x] === "ob-se" || imgArray[y+1][x] === "oc-ne" || imgArray[y+1][x] === "oa-ne") {
-								if (imgArray[y][x-1] === "o-n" || imgArray[y][x-1] === "ob-nw" || imgArray[y][x-1] === "oc-ne" || imgArray[y][x-1] === "oa-ne" ) {
-									imgArray[y][x] = "ob-ne";
-								}
-							}
-							if ((imgArray[y+1][x-1] === "o-e" || imgArray[y+1][x-1] === "ob-ne" || imgArray[y+1][x-1] === "oc-se" || imgArray[y+1][x-1] === "oa-nw") && (imgArray[y][x-1] === "o-n" || imgArray[y][x-1] === "ob-nw" || imgArray[y][x-1] === "oc-ne" || imgArray[y][x-1] === "oa-ne")) {
-								imgArray[y][x] = "ob-ne";
-							}
+							imgArray[y][x] = "o-e";
 						}
 					}
-					if ((s && (w || nw)) || (w && se)){
-						if (x === 6 && y === 0) {
-							imgArray[y][x] = "ob-sw";
-						} else if (x === 6) {
-							if (imgArray[y-1][x] === "o-w" || imgArray[y-1][x] === "ob-nw" || imgArray[y-1][x] === "oc-sw" || imgArray[y-1][x] === "oa-ne") {
-								imgArray[y][x] = "ob-sw";
-							}
-						} else if (y === 0) {
-							if (imgArray[y][x+1] === "o-s" || imgArray[y][x+1] === "ob-se" || imgArray[y][x+1] === "oc-sw" || imgArray[y][x+1] === "oa-ne" ) {
-								imgArray[y][x] = "ob-sw";
-							}
-						} else {
-							if (imgArray[y-1][x] === "o-w" || imgArray[y-1][x] === "ob-nw" || imgArray[y-1][x] === "oc-sw" || imgArray[y-1][x] === "oa-ne") {
-								if (imgArray[y][x+1] === "o-s" || imgArray[y][x+1] === "ob-se" || imgArray[y][x+1] === "oc-sw" || imgArray[y][x+1] === "oa-ne" ) {
-									imgArray[y][x] = "ob-sw";
-								}
-							}
-						}
-					}
-					if ((s && (e || ne)) || (e && sw)){
-						if (x === 0 && y === 0) {
-							imgArray[y][x] = "ob-se";
-						} else if (x === 0) {
-							if (imgArray[y-1][x] === "o-e" || imgArray[y-1][x] === "ob-ne" || imgArray[y-1][x] === "oc-se" || imgArray[y-1][x] === "oa-nw") {
-								imgArray[y][x] = "ob-se";
-							}
-						} else if (y === 0) {
-							if (imgArray[y][x-1] === "o-s" || imgArray[y][x-1] === "ob-sw" || imgArray[y][x-1] === "oc-se" || imgArray[y][x-1] === "oa-nw" ) {
-								imgArray[y][x] = "ob-se";
-							}
-						} else {
-							if (imgArray[y-1][x] === "o-e" || imgArray[y-1][x] === "ob-ne" || imgArray[y-1][x] === "oc-se" || imgArray[y-1][x] === "oa-nw") {
-								if (imgArray[y][x-1] === "o-s" || imgArray[y][x-1] === "ob-sw" || imgArray[y][x-1] === "oc-se" || imgArray[y][x-1] === "oa-nw" ) {
-									imgArray[y][x] = "ob-se";
-								}
-							}
-						}
-					}
+					if (check(y, x-1) === "o") imgArray[y][x] = "o-w";
+				}
+				x++;
+			}
+			x = 0;
+			y++;
+		}
+		// Fixes #2
+		x = 0;
+		y = 0;
+		while (y < 7) {
+			while (x < 7) {
+				if (check(y, x) === "oc-ne") {
+					if (check(y, x-1) === "o-s") imgArray[y][x] = "oa-ne";
+					if (check(y+1, x) === "o-w") imgArray[y][x] = "oa-ne";
+				}
+				if (check(y, x) === "oc-nw") {
+					if (check(y, x+1) === "o-s") imgArray[y][x] = "oa-nw";
+					if (check(y+1, x) === "o-e") imgArray[y][x] = "oa-nw";
 				}
 				x++;
 			}

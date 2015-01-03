1b24ebeb38b7668ef10eb9b6309f83d305ae7856
Cedric Bondoc <jced826@gmail.com>
2015-01-03 17:43:51 +0000

    Military actions

--- handler/military_handler.py
+++ handler/military_handler.py
@@ -18,9 +18,46 @@ class MilitaryHandler(DefaultHandler):
             "num"    : num
         }
 
+        if self.user.military = False:
+            self.stderr('You don\'t have military building')
+        elif self.user.peopleAtMilitary = 0:
+            self.stderr('You don\'t have enough people to attack')
+        else:
+            self.user.updateValues()
+            user = User.query(User.uid==city).fetch()[0]
+            user.updateValues()
+            user.peopleAtHome = user.peopleAtHome - ((random.randint(39, 80)/100) * user.peopleAtHome)
+            #Stolen resources
+            wood = user.wood - (5% * user.storageLvl) * user.wood
+            gold = user.gold - (5% * user.storageLvl) * user.gold
+            food = user.food - (5% * user.storageLvl) * user.food
+            stone = user.stone - (5% * user.storageLvl) * user.stone
+
+            #Add to the user
+            self.user.wood +=  wood
+            self.user.gold +=  gold
+            self.user.food +=  food
+            self.user.stone += stone
+
+            #Minus the attacked city
+            user.wood -=  wood
+            user.gold -=  gold
+            user.food -=  food
+            user.stone -= stone
+
     def send(self, city, num):
         self.json = {
             "action" : "send",
             "city" : city,
             "num"  : num
         }
+        if self.user.military = False:
+            self.stderr('You don\'t have military building')
+        elif self.user.peopleAtMilitary < num:
+            self.stderr('You don\'t have enough people')
+        else:
+            user = User.query(User.uid==city).fetch()[0]
+            user.peopleAtMilitary += num
+            self.user.peopleAtMilitary -= num
+
+
